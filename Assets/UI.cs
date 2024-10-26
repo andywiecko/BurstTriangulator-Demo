58b588637fd72b4781311916356b37daa1efd8d4
andywiecko <andywiecko@gmail.com>
2024-10-26 16:44:53 +0200

    feat: display package version in ui

--- Assets/UI.cs
+++ Assets/UI.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,6 +9,9 @@ namespace andywiecko.BurstTriangulator.Demo
     {
         [SerializeField]
         private TriangulatorComponent target = default;
+
+        [SerializeField]
+        string packageVersion = "vx.x.x";
 
         private Label trianglesCount, pointsCount;
 
@@ -52,6 +56,7 @@ namespace andywiecko.BurstTriangulator.Demo
                 Run();
             });
 
+            root.Q<VisualElement>("version").Q<Label>("count").text = packageVersion;
             trianglesCount = root.Q<VisualElement>("stats-triangles").Q<Label>("count");
             trianglesCount.text = (target.Output.Triangles.Length / 3).ToString();
             pointsCount = root.Q<VisualElement>("stats-points").Q<Label>("count");
@@ -64,5 +69,41 @@ namespace andywiecko.BurstTriangulator.Demo
             trianglesCount.text = (target.Output.Triangles.Length / 3).ToString();
             pointsCount.text = target.Output.Positions.Length.ToString();
         }
+
+#if UNITY_EDITOR
+        UnityEditor.PackageManager.Requests.SearchRequest request;
+        const int requestLimit = 100;
+        int count = 0;
+
+        private void OnValidate()
+        {
+            if (Application.isPlaying)
+            {
+                return;
+            }
+
+            count = 0;
+            request = UnityEditor.PackageManager.Client.Search("com.andywiecko.burst.triangulator");
+            UnityEditor.EditorApplication.update += CheckVersionRequest;
+        }
+
+        private void CheckVersionRequest()
+        {
+            count++;
+
+            if (request.IsCompleted)
+            {
+                Debug.Log(request.Result.First().version);
+                packageVersion = "v" + request.Result.First().version;
+                UnityEditor.EditorApplication.update -= CheckVersionRequest;
+            }
+
+            if (count == requestLimit)
+            {
+                Debug.LogError("Check package version request failed.");
+                UnityEditor.EditorApplication.update -= CheckVersionRequest;
+            }
+        }
+#endif
     }
 }
